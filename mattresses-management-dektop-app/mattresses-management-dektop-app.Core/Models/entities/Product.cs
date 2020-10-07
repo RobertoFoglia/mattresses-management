d2b10e47f58d7554bff19392d90a9beedb2ef1d7
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2020-10-07 12:37:44 +0200

    Main page improvements
    - The replacing of the content view
    - Propeties for the DataGrid

--- mattresses-management-dektop-app/mattresses-management-dektop-app.Core/Models/entities/Product.cs
+++ mattresses-management-dektop-app/mattresses-management-dektop-app.Core/Models/entities/Product.cs
@@ -11,5 +11,7 @@ namespace mattresses_management_dektop_app.Core.Models.entities
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
+        public String Description { get; set; }
+        public Double Price { get; set; }
     }
 }
