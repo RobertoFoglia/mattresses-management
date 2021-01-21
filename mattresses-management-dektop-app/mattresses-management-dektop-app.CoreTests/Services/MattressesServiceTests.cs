79ccfa8e47f596b1d7a80c4e58cdee4d3c80536d
Roberto Foglia <roberto.foglia.computer.scientist@gmail.com>
2021-01-21 21:30:52 +0100

    #19 Junit installation

---
+++ mattresses-management-dektop-app/mattresses-management-dektop-app.CoreTests/Services/MattressesServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using mattresses_management_dektop_app.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mattresses_management_dektop_app.Core.Services.Tests
+{
+    [TestClass()]
+    public class MattressesServiceTests
+    {
+        [TestMethod()]
+        public void CalculateTheAttributesTest()
+        {
+            var service = new MattressesService(null, null, null, null);
+            var mattress = new Models.entities.Mattress();
+            mattress.Attributes = new List<Models.entities.Attribute>();
+            mattress.Products = new List<Models.entities.Product>();
+            mattress.Products.Add(new Models.entities.Product());
+            service.CalculateTheAttributes(mattress);
+            Assert.Fail();
+        }
+    }
+}
